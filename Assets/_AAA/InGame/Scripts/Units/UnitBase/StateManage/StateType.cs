e4939a2abcc7c7c9d891d73984a92c6e115bc8dd
tojinguyen <toainguyenprogramminggame@gmail.com>
2024-12-28 23:54:18 +0700

    update states

--- Assets/_AAA/InGame/Scripts/Units/UnitBase/StateManage/StateType.cs
+++ Assets/_AAA/InGame/Scripts/Units/UnitBase/StateManage/StateType.cs
@@ -6,21 +6,10 @@ public enum StateType : byte
 
     Move = 5,
 
-    NormalAttack = 20,
-
-
     Skill = 50,
-
-    Defense = 80,
-
 
     Damaged = 100,
     KnockBack = 101,
-
-    Invulnerable = 110,
-
-    DodgeFront = 150,
-    DodgeBack = 151,
 
     Dead = 200,
     Respawn = 201,
