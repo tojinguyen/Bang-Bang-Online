b5b0360880f97af9a0756f3ea63a6adbf89f14e7
tojinguyen <toainguyenprogramminggame@gmail.com>
2025-01-04 09:28:36 +0700

    add layer tank

--- Assets/_AAA/InGame/Scripts/Units/Tanks/Client/TankSkinRenderer.cs
+++ Assets/_AAA/InGame/Scripts/Units/Tanks/Client/TankSkinRenderer.cs
@@ -1,21 +1,48 @@
+using System;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class TankSkinRenderer : MonoBehaviour
 {
     [SerializeField] private TanksConfig _tanksConfig;
     [SerializeField] private Transform _visualRoot;
-    
+
     public async UniTask SetupSkin(TankType tankType)
     {
         var tankConfig = _tanksConfig.GetTankConfig(tankType);
         var tankSkinAssetRef = tankConfig.TankRendererConfig.SkinAssetRef;
 
-        var tankSkinGo = await AddressablesHelper.GetAssetAsync<GameObject>(tankSkinAssetRef, AddressablesFeatureName.InGame);
-        
+        var tankSkinGo =
+            await AddressablesHelper.GetAssetAsync<GameObject>(tankSkinAssetRef, AddressablesFeatureName.InGame);
+
         tankSkinGo.transform.SetParent(_visualRoot);
         tankSkinGo.transform.localPosition = Vector3.zero;
         tankSkinGo.transform.localRotation = Quaternion.identity;
         tankSkinGo.SetActive(true);
     }
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        if (_tanksConfig == null)
+        {
+            // Load all TankConfig assets in the project
+            var tankConfigs = AssetDatabase.FindAssets("t:tanksconfig");
+            var configs = new TankConfig[tankConfigs.Length];
+            if (configs == null) throw new ArgumentNullException(nameof(configs));
+
+            for (var i = 0; i < configs.Length; i++)
+            {
+                var path = AssetDatabase.GUIDToAssetPath(tankConfigs[i]);
+                _tanksConfig = AssetDatabase.LoadAssetAtPath<TanksConfig>(path);
+            }
+
+            // Mark the ScriptableObject as dirty to ensure changes are saved
+            EditorUtility.SetDirty(this);
+        }
+    }
+#endif
 }
