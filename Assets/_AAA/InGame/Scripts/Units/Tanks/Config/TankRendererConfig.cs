d1176673f5d9938aaefcf9400e4687b8851574c0
tojinguyen <toainguyenprogramminggame@gmail.com>
2025-01-04 08:48:22 +0700

    update config skin

--- Assets/_AAA/InGame/Scripts/Units/Tanks/Config/TankRendererConfig.cs
+++ Assets/_AAA/InGame/Scripts/Units/Tanks/Config/TankRendererConfig.cs
@@ -1,29 +1,8 @@
 using UnityEngine;
-using System.Collections.Generic;
+using UnityEngine.AddressableAssets;
 
 [CreateAssetMenu(fileName = "TankRendererConfig", menuName = "AAA/InGame/Unit/Tanks/TankRendererConfig")]
 public class TankRendererConfig : ScriptableObject
 {
-    [System.Serializable]
-    public class TankSkin
-    {
-        public SkinType SkinType; 
-        public GameObject prefab;
-        public Material Material;
-    }
-
-    [SerializeField] private List<TankSkin> _tankSkins;
-}
-
-public enum SkinType
-{
-    Default = 0,
-    Golden = 1,
-    Silver = 2,
-    Stealth = 3,
-    Lava = 4,
-    Ice = 5,
-    Galaxy = 6,
-    Neon = 7,
-    Shadow = 8,
+    [SerializeField] private AssetReferenceGameObject _tankSkin;
 }
